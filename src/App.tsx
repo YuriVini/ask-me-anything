8af963016ec52dafa05f7a8ed39e652d8bfb865b
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-09 01:39:20 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { CreateRoom } from "./pages/create-room"
+import { Room } from "./pages/room"
+
 export const App = () => {
 
+  const router  = createBrowserRouter([
+    {
+      path: "/",
+      element: <CreateRoom />,
+    },
+    {
+      path: "/room/:roomId",
+      element: <Room />,
+    }
+  ])
+
   return (
-    <p>
-      Oi
-    </p>
+    <RouterProvider router={router}/>
   )
 }
 
