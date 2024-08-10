6e56bd987a57ee2839164bcc70bceca24fea6461
YuriVini <yuri.ybgt.yfo@gmail.com>
2024-08-10 12:51:06 -0300

    feat: implementing input form component

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom"
-import { ArrowRight, Share2 } from "lucide-react"
+import { Share2 } from "lucide-react"
 import { toast } from "sonner"
 
 import amaLogo from "../assets/ama-logo.svg"
-import { Message } from "../components/message"
+import { CreateMessageForm } from "../components/create-message-form"
 
 export const Room = () => {
     const { roomId } = useParams()
@@ -48,24 +48,7 @@ export const Room = () => {
 
             <div className="h-px w-full bg-zinc-900"/>
 
-            <form action={handleCreateQuestion} className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950  focus-within:ring-1">
-                <input 
-                    type="text" 
-                    name="theme" 
-                    autoComplete="off" 
-                    placeholder="What's your question?" 
-                    className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-                />
-
-                <button type="submit" className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500">
-                    Create question
-                    <ArrowRight className="size-4"/>
-                </button>
-            </form>
-
-            <ol className="list-decimal list-outside px-3 space-y-8">
-                <Message answered amountOfReactions={123} text="O que é GoLang e quais são suas principais vantagens em comparação com outras linguagens de programação como Python, Java ou C++?" />
-            </ol>
+            <CreateMessageForm />
         </div>
     )
 }
